96d95ab98bc23bf231b41e5c840bc0b0d855bfbf
ryzerrv2 <mariovicidomini7@gmail.com>
2023-05-10 08:45:14 +0200

    english support

--- utils/changeLang.js
+++ utils/changeLang.js
@@ -3,28 +3,20 @@ const itFlag = document.getElementById("it-flag");
 
 ukFlag.addEventListener("click", () => {
 	const url = window.location.href;
-
-	const domain = url.substring(0, url.indexOf('.it') + 3) + '/en/';
-
-	const indexActPage = url.lastIndexOf('/') + 1; 
-
-	const actPage = url.substring(indexActPage);
-					
+	if(url.indexOf('en') > -1) return;
+	const domain = url.substring(0, url.indexOf('.it') + 3);
+	const indexActPage = url.indexOf('.it') + 3; 
+	const actPage = '/en' + url.substring(indexActPage);
 	const newUrl = domain + actPage;
-
 	window.location.href = newUrl;
 });
 
 itFlag.addEventListener("click", () => {
 	const url = window.location.href;
-
+	if(url.indexOf('en') === -1) return;
 	const domain = url.substring(0, url.indexOf('.it') + 3);
-
-	const indexActPage = url.lastIndexOf('/') + 1; 
-
+	const indexActPage = url.indexOf('.it') + 6; 
 	const actPage = url.substring(indexActPage);
-					
 	const newUrl = domain + actPage;
-
 	window.location.href = newUrl;
 });
